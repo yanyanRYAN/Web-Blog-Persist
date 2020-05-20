197878cf3634c4dffde8560c175e1f63307b560d
yanyanRYAN <rpecarma@gmail.com>
2020-05-20 13:08:08 -0500

    finished

---
+++ app.js
@@ -0,0 +1,149 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require('lodash');
+const mongoose = require('mongoose');
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const posts = [];
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+//--connect to db
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+//--check for connection to db
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log("We're Connected!");
+});
+
+//--create schema
+const postSchema = new mongoose.Schema({
+  title: String,
+  body: String
+})
+
+//--create model
+const Post = mongoose.model('Post', postSchema);
+
+
+app.get("/", function(req, res){
+
+  Post.find(function(err, posts){
+    if(err){
+      console.log(err);
+    } else {
+      res.render('home', {
+        homePageContent: homeStartingContent,
+        postArray: posts
+      })
+    }
+  });
+
+  // res.render('home', {homePageContent: homeStartingContent,
+  // postArray: posts});
+  // console.log(posts[0].title);
+  // console.log();
+
+
+
+})
+
+app.get("/about", function(req, res){
+  res.render('about', {aboutPageContent: aboutContent });
+})
+
+app.get("/contact", function(req, res){
+  res.render('contact', {contactPageContent: contactContent});
+})
+
+app.get("/compose", function(req, res){
+
+  res.render('compose');
+
+})
+
+app.post("/compose", function(req,res){
+
+  // const post = {
+  //   title: req.body.postTitle,
+  //   entry: req.body.postEntry
+  // };
+
+  // console.log(req.body.postTitle);
+  // console.log(req.body.postEntry);
+
+  //console.log(post);
+  //posts.push(post);
+
+  const post = new Post({
+    title: req.body.postTitle,
+    body: req.body.postEntry
+  })
+
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    } else {
+      console.log(err);
+    }
+  });
+
+
+})
+
+// routing params --dynamic url
+app.get("/posts/:postId", function(req,res){
+  console.log("FULL URL: /posts/" + req.params.postId);  // req.params.paramName
+  const requestedPost = req.params.postId;
+
+  Post.findById(requestedPost, function(err, foundPost){
+    if(!err){
+      console.log(foundPost);
+      res.render('post',{
+        title: foundPost.title,
+        body: foundPost.body
+      })
+    } else {
+      console.log(err);
+    }
+  })
+
+
+  // posts.forEach(function(post){
+  //   const storedPost = _.lowerCase(post.title);
+  //   if(storedPost === requestedPost){
+  //     console.log("match found");
+  //     res.render('post',{
+  //       title: post.title,
+  //       entry: post.entry,
+  //     })
+  //   } else {
+  //     console.log("no match");
+  //   }
+  //   console.log("stored post: " + storedPost);
+  //   console.log("_.lowerCase(storedPost): " + _.lowerCase(storedPost));
+  //   console.log("requested post: " + requestedPost );
+  //
+  // });
+
+
+})
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
